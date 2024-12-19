9ea6256bbd7b4ee03d8b27b157d1a5f6b2d8c9b7
om mandaliya <ommandaliya6@gmail.com>
2024-12-19 09:45:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
-$num1 = 10;
-$num2 = 20;
-echo "First Number Is :- " , $num1;
-echo "<br>";
-echo "Second Number Is :- " , $num2;
-echo "<br>";
-$sum = $num1 + $num2;
-echo "The sum of $num1 and $num2 is: $sum";
+$num = 7;
+if ($num % 2 == 0) {
+    echo "$num is an even number.";
+} else {
+    echo "$num is an odd number.";
+}
 ?>
