79b16d43956beb57dcebe7c2cbfc9909fc6b3914
om mandaliya <ommandaliya6@gmail.com>
2024-12-19 09:44:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$num1 = 10;
+$num2 = 20;
+echo "First Number Is :- " , $num1;
+echo "<br>";
+echo "Second Number Is :- " , $num2;
+echo "<br>";
+$sum = $num1 + $num2;
+echo "The sum of $num1 and $num2 is: $sum";
+?>
